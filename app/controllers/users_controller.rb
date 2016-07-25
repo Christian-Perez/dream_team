b7d9e8ec3b997fde97f87eb6a36354bb25dd3356
Christian Perez <chris.dillon92@gmail.com>
2016-07-25 11:08:56 -0700

    # <- Subject message. Maximum 50 characters.   ->| # # <- After two line breaks come the rest of the lines, or the body.  ->| # <- The maximum line length for the body is 72 characters.          ->| # # For more info on how to write a good commit message, see: # http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html WIP adding CRUD to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,51 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # how do i find the params for members(users)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.update_attributes(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+
+    @user = User.find(params[:id])
+
+  if @user.update_attributes(user_params)
+    redirect_to user_path
+  else
+    render :edit
+  end #if block
+
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :tag, :mission, :image)
+  end
 end
